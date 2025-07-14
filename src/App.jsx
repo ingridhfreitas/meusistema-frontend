b4cc159d7ff7758a8cd06fed90dfd4d36b8edf81
ingridhfreitas <ingridhpfreitas@gmail.com>
2025-07-14 20:03:32 -0300

    fixed: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicial from "./pages/Inicial";
 import FornecedorForm from "./pages/Fornecedor/FornecedorForm";
+import Menu from "./components/Menu";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <FornecedorForm />
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Inicial />} />
+        <Route path="/cadastrar-fornecedor" element={<FornecedorForm />} />
+      </Routes>
     </BrowserRouter>
   );
 };
